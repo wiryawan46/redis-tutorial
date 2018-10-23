5435f4b958db971f53e8b84142d373a7d10d1a77
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2018-10-23 09:09:43 +0700

    Initial commit

---
+++ src/main/java/id/wiryawan/redistutorial/RedisTutorialApplication.java
@@ -0,0 +1,12 @@
+package id.wiryawan.redistutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisTutorialApplication.class, args);
+    }
+}
