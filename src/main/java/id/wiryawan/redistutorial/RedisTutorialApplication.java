6354455313900ccb4445085605f87abf7fdd214e
Manggala Pramuditya Wiryawan <manggala.wiryawan@gmail.com>
2018-10-25 15:54:46 +0700

    Last commit

--- src/main/java/id/wiryawan/redistutorial/RedisTutorialApplication.java
+++ src/main/java/id/wiryawan/redistutorial/RedisTutorialApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisTutorialApplication {
+    
+    
 
     public static void main(String[] args) {
         SpringApplication.run(RedisTutorialApplication.class, args);
---
+++ src/main/java/id/wiryawan/redistutorial/config/RedisConfig.java
@@ -0,0 +1,31 @@
+package id.wiryawan.redistutorial.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName("localhost");
+        jedisConnectionFactory.setPort(6379);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    RedisTemplate<String, Object> getRedisTemplate() {
+        RedisTemplate redisTemplate = new RedisTemplate();
+        redisTemplate.setConnectionFactory(jedisConnectionFactory());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+
+}
